5c2b29fc0e542748d55c9b441fa0436b86d024b9
James Barry <barryj@flip1.engr.oregonstate.edu>
2017-11-16 22:08:22 -0800

    new file, updated some

--- connectCredentials.php
+++ connectCredentials.php
@@ -1,7 +1,7 @@
 <?php
   // Define database connection constants
   define('DB_HOST', 'classmysql.engr.oregonstate.edu');
-  define('DB_USER', 'cs340_fernanen');
-  define('DB_PASSWORD', '1305');
-  define('DB_NAME', 'cs340_fernanen');
+  define('DB_USER', 'cs340_barryj');
+  define('DB_PASSWORD', '4339');
+  define('DB_NAME', 'cs340_barryj');
 ?>
---
+++ game.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!-- Show Game -->
+<html>
+	<head>
+		<title>Show Game</title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+<body>
+<?php
+
+	include 'connectCredentials.php'; 
+	
+	$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+	if (!$conn) {
+		die('Could not connect: ' . mysql_error());
+	}
+	
+	$userinput = mysqli_real_escape_string($conn, $_POST['gameID']);
+	$query = "SELECT gameName FROM Games WHERE gameID = $userinput";
+	$result = mysqli_query($conn, $query);
+	$row = mysqli_fetch_row($result);
+	
+	echo "<h2> You selected $row[0] </h2>"
+	
+?>
+	
+</form>
+</body>
+</html>
