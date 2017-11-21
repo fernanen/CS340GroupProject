88ee06978f7de108cf8eaa5c5191b10337b7de4c
luoja <luoja@oregonstate.edu>
2017-11-20 21:44:13 -0800

    Update updateBioPage.php

--- updateBioPage.php
+++ updateBioPage.php
@@ -9,15 +9,12 @@
 					<link rel="stylesheet" type="text/css" href="myStyle.css">
 	</head>
 <body class = "Update">
-	<?php
-		print_r($_SESSION);
-	?>
 <a href = ./mainPage.php>Home</a>
 <h2 class = "Update">Write your update user bio below:</h2>
 <form name = "updateContent" class = "Bio" action = "updateBio.php" method = "post" >
 		<p>
 			<label for="Bio">Bio:<label>
-			<input type="text" name = "bio" id = "bio" placeHolder = "bio">
+			<input type="text" name = "bio" id = "bio" placeHolder = "New Bio">
 		</p>
 	<input type = "submit" value = "submit">
 </form>
