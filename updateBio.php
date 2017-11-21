d34f99dc7cd009134ec0368ebf5cb9d78d71322d
James Luo <luoja@flip1.engr.oregonstate.edu>
2017-11-20 21:10:23 -0800

    UpdateBio stuff

---
+++ updateBio.php
@@ -0,0 +1,52 @@
+<?php
+    session_start();
+ ?>
+<!DOCTYPE html>
+<!--Update user's bio-->
+<html>
+	<head>
+		<title>Register</title>
+	</head>
+<body>
+<a href = ./mainPage.php> Home </a>
+<?php
+	// Get login Credentials Using this file
+	include 'connectCredentials.php';
+
+	// attempt to connect to database
+	$connected = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+	//check if connected
+	if(!$connected)
+	{
+		die('Could Not Connect: ' . mysql_error());
+	}
+
+	//Escape user Input for Security
+	//$UserName = mysqli_real_escape_string($connected, $_POST['UserName']);
+    $bio = mysqli_real_escape_string($connected, $_POST['bio']);
+
+
+    echo "userName".$_SESSION["userName"];
+
+	//attempt update query
+    print_r($_SESSION);
+	$query = "UPDATE Users
+            SET bio = '$bio'
+            WHERE username = '$_SESSION["userName"]'";
+
+//    $query = "UPDATE Users SET bio = '$bio' WHERE username = '$UserName'";
+	//check if query was successful
+    if(mysqli_query($connected, $query))
+	{
+            echo "Record added successfully.";
+    } else
+	{
+            echo "ERROR: Could not able to execute $query. " . mysqli_error($connected);
+    }
+
+	// close connection
+	mysqli_close($connected);
+?>
+</body>
+</html>
---
+++ updateBioPage.php
@@ -0,0 +1,25 @@
+<<?php
+	session_start();
+ ?>
+<!DOCTYPE html>
+<!-- Get new User Info-->
+<html>
+	<head>
+		<title>Register</title>
+					<link rel="stylesheet" type="text/css" href="myStyle.css">
+	</head>
+<body class = "Update">
+	<?php
+		print_r($_SESSION);
+	?>
+<a href = ./mainPage.php>Home</a>
+<h2 class = "Update">Write your update user bio below:</h2>
+<form name = "updateContent" class = "Bio" action = "updateBio.php" method = "post" >
+		<p>
+			<label for="Bio">Bio:<label>
+			<input type="text" name = "bio" id = "bio" placeHolder = "bio">
+		</p>
+	<input type = "submit" value = "submit">
+</form>
+</body>
+</html>
