be8698ebcbcd3286956c0a878a424e00f14f1836
James Luo <luoja@flip1.engr.oregonstate.edu>
2017-11-20 22:40:13 -0800

    Added temp My Review page

--- myReviews.php
+++ myReviews.php
@@ -1,15 +1,15 @@
 <?php
 session_start();
 ?>
-<!DOCTYPE html> 
-<!--Insert Into HW1 Users Table CS 340--> 
-<html> 
+<!DOCTYPE html>
+<!--Insert Into HW1 Users Table CS 340-->
+<html>
 	<head>
 		<title>Register</title>
 		<link rel="stylesheet" type="text/css" href="myStyle.css">
-	</head> 
+	</head>
 <body>
-	<div class = "navbar"> 
+	<div class = "navbar">
 		<a href = ./mainPage.php > Home </a>
 		<div class = "dropdown">
 			<button class = "dropbtn">Account
@@ -21,7 +21,48 @@ session_start();
 				<a href = ./updateBioPage.php>Update Bio </a>
 			</div>
 		</div>
-		<a href = ./about.php> About </a> 
+		<a href = ./about.php> About </a>
 	</div>
-</body> 
-</html> 
+	<?php
+		include 'connectCredentials.php';
+
+			$connected = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+			if (!$connected) {
+					die('Could not connect: ' . mysql_error());
+			}
+		//Retrieve table
+		$uname = $_SESSION["userName"];
+		$query = "SELECT gameName, reviewContent, score, datePosted FROM UserReview NATURAL JOIN Games WHERE username = '$uname' ORDER BY gameID";
+
+
+		$result = mysqli_query($connected,$query);
+		if(!$result)
+		{
+			die("Query to show fields from table failed");
+		}
+
+		// get number of columns in the table
+		$fieldNum = mysqli_num_fields($result);
+		echo"<h1> $table </h1>";
+		echo"<table id='t01' border = '1'><tr>";
+		//printing table headers
+		for($x=0; $x < $fieldNum; $x++)
+		{
+			$field = mysqli_fetch_field($result);
+			echo "<td><b>$field->name</b></td>";
+		}
+		echo "<tr>\n";
+		while($row = mysqli_fetch_row($result))
+		{
+					echo "<tr>";
+					// $row is array... foreach( .. ) puts every element
+					// of $row to $cell variable
+					foreach($row as $cell)
+							echo "<td>$cell</td>";
+					echo "</tr>\n";
+			}
+			mysqli_free_result($result);
+			mysqli_close($conn);
+	?>
+</body>
+</html>
