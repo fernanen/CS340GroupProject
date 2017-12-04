fcd70a279b6858c596853720e7d940b19acd8855
James Barry <barryj@flip3.engr.oregonstate.edu>
2017-12-04 15:05:09 -0800

    new stuff

--- myAccount.php
+++ myAccount.php
@@ -1,11 +1,29 @@
 <?php include("./header.php");?>
 <?php
-		if($_SESSION["userName"] != ""){
-			echo "<h2>Username: ". $_SESSION["userName"] ."</h2>";
-			echo "<h2>First Name: ".$_SESSION["firstName"]."</h2>";
-			echo "<h2>Last Name: ".$_SESSION["lastName"]."</h2>";
-			echo "<h2>Age: ".$_SESSION["age"]."</h2>";
-			echo "<h2>Bio: ".$_SESSION["bio"]."</h2>";
+		if($_SESSION["userName"] != ""){			
+			include 'connectCredentials.php';
+
+			$connected = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+			//check if connected
+			if(!$connected)
+			{
+				die('Could Not Connect: ' . mysql_error());
+			}
+			
+			$username = $_SESSION['userName'];
+			
+			$queryTodo = "SELECT * FROM Users WHERE userName = '$username'";
+			$query = mysqli_query($connected,$queryTodo);
+			if(!$query)
+			{
+				echo "QUERY FAILED";
+			}
+
+			$row = $query->fetch_row();
+			
+			echo 	"<div class =\"outwrapper\"><div class=\"textwrapper\">
+					<li class = \"profile\">Username: $row[0]</li>";
+			
 		}
 		else{
 			echo "You are not logged in!";
