5cbcac5ef0e08cea904699bfb5d070a79b428e5f
James Barry <barryj@flip1.engr.oregonstate.edu>
2017-12-03 20:38:52 -0800

    new main and style

--- mainPage.php
+++ mainPage.php
@@ -23,9 +23,37 @@
 		
 		while($row = mysqli_fetch_row($result))
 		{
-			echo 	"<li class = \"gamewrapper\">
-					<div class = \"artwrapper\"><img class = \"boxart\" src = \"$row[6]\"></div><br>
-					<a href=./game.php?ID=" . $row[0] . ">$row[1]</a></li>";
+			echo "<li class = \"gamewrapper\">";
+			
+			if($row[4] > 70){
+				echo "<div class = \"goodscore score critic\">Critic<br>$row[4]</div>";
+			}
+			
+			elseif($row[4] > 50){
+				echo "<div class = \"mixedscore score critic\">Critic<br>$row[4]</div>";
+			}
+			
+			else{
+				echo "<div class = \"badscore score critic\">Critic<br>$row[4]</div>";
+			}
+			
+			if($row[5] > 70){
+				echo "<div class = \"goodscore score user\">User<br>$row[5]</div>";
+			}
+			
+			elseif($row[4] > 50){
+				echo "<div class = \"mixedscore score user\">User<br>$row[5]</div>";
+			}
+			
+			else{
+				echo "<div class = \"badscore score user\">User<br>$row[5]</div>";
+			}
+					
+			echo "	<div class = \"artwrapper\"><img class = \"boxart\" src = \"$row[6]\"></div><br>
+					<a href=./game.php?ID=" . $row[0] . ">$row[1]</a>";
+			
+			echo "</li>";
+			
 		}
 		
 		echo "</div>";
