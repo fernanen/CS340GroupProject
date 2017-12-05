88979a2369f1df309130e8c20b4fbe5dccf6e0ad
James Barry <barryj@flip3.engr.oregonstate.edu>
2017-12-04 16:09:53 -0800

    new stuff

--- myReviews.php
+++ myReviews.php
@@ -8,7 +8,7 @@
 			}
 		//Retrieve table
 		$uname = $_SESSION["userName"];
-		$query = "SELECT gameName, reviewContent, score, datePosted FROM UserReview NATURAL JOIN Games WHERE username = '$uname' ORDER BY gameID";
+		$query = "SELECT gameName, reviewContent, score, datePosted, gameID FROM UserReview NATURAL JOIN Games WHERE username = '$uname' ORDER BY gameID";
 
 
 		$result = mysqli_query($connected,$query);
@@ -19,26 +19,37 @@
 
 		// get number of columns in the table
 		$fieldNum = mysqli_num_fields($result);
-		echo"<h1> $table </h1>";
-		echo"<table id='t01' border = '1'><tr>";
+		echo"<div class =\"outwrapper\"><div class=\"textwrapper\">";
+		
+		echo"<table><tr>";
 		//printing table headers
-		for($x=0; $x < $fieldNum; $x++)
-		{
-			$field = mysqli_fetch_field($result);
-			echo "<td><b>$field->name</b></td>";
-		}
+
 		echo "<tr>\n";
 		while($row = mysqli_fetch_row($result))
 		{
-					echo "<tr>";
-					// $row is array... foreach( .. ) puts every element
-					// of $row to $cell variable
-					foreach($row as $cell)
-							echo "<td>$cell</td>";
-					echo "</tr>\n";
+			echo 	"<table>
+					<tr>
+						<td><b>Game:</b></td>
+						<td><a href=./game.php?ID=" . $row[4] . ">$row[0]</a></td>
+					</tr>
+					<tr>
+						<td><b>Review:</b></td>
+						<td>$row[1]</td>
+					</tr>
+					</table>";
+			echo	"<tr>
+						<td><b>Score:</b></td>
+						<td class = \"rawscore\">$row[2]</td>
+					</tr>";
+			echo	"<tr>
+						<td><b>Date:</b></td>
+						<td>$row[3]</td>
+					</tr>
+					</table>
+					<br>";
 			}
-			mysqli_free_result($result);
-			mysqli_close($conn);
+		mysqli_free_result($result);
+		mysqli_close($conn);
 	?>
 </body>
 </html>
